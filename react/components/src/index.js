ec15054adb4806f37cc8f160b72f4d549a2fa7be
Rashpal Gill <rsgillit@gmail.com>
2019-05-21 18:15:44 -0500

    updates commit

--- react/components/src/index.js
+++ react/components/src/index.js
@@ -2,30 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from "faker";
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 
 
 const App = function () {
     return (
         <div className="ui container comments">
-           <CommentDetail
-               author="Sam"
-               timeAgo="Today at 4:55PM"
-               comment="Nice!"
-               image={faker.image.avatar()}
-           />
-           <CommentDetail
-               author="Alex"
-               timeAgo="Today at 2:00AM"
-               comment="well done!"
-               image={faker.image.avatar()}
-           />
-           <CommentDetail
-               author="Jane"
-               timeAgo="yesterday at 5:00PM"
-               comment="wow!"
-               image={faker.image.avatar()}
-           />
+
+
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 4:55PM"
+                    comment="Nice!"
+                    image={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 2:00AM"
+                    comment="well done!"
+                    image={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="yesterday at 5:00PM"
+                    comment="wow!"
+                    image={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <div>
+                        <h4>Warning!</h4>
+                </div>
+                Are you sure you want to do this ?
+            </ApprovalCard>
+
+
         </div>
     );
 
