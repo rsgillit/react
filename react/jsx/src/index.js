0c4d0ee9dd083d3655d65c210d63a14dd39b0f04
Rashpal Gill <rsgillit@gmail.com>
2019-05-21 16:17:59 -0500

    first component

---
+++ react/jsx/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ 
+
+const App = function () {
+    const buttonText = 'Click Me!';
+    return (
+        <div>
+            <label className="label" for="name">Enter name:</label>
+            <input id="name" type="text"/>
+            <button style={{ backgroundColor: 'blue', color: 'white'}}>
+                {buttonText}
+            </button>
+
+        </div>
+    );
+    
+};
+
+ReactDOM.render(
+    <App />,
+        document.querySelector('#root')
+);
