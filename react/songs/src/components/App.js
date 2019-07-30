045d7df4988e47699b13e8da41c9f8f5d8fe278b
Rashpal Gill <rsgillit@gmail.com>
2019-07-30 13:26:09 -0500

    another one

---
+++ react/songs/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const App = function () {
+    return (
+        <div> test
+        </div>
+    );
+};
+
+export default App;
---
+++ react/songs/src/components/SongList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+class SongList extends React.Component {
+    
+}
